5339e15ea8f4d50f4a3e1706aa3fe83f9c8aafcd
Tu Nguyen <127782708+tunguyenn99@users.noreply.github.com>
2026-03-10 01:20:23 +0700

    Time: 415 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    p.firstName
+    , p.lastName
+    , a.city
+    , a.state
+FROM person p
+LEFT JOIN address a
+    ON a.personId = p.personID

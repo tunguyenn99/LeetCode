850aec79c58379dd39d5353d86cdf0a631a83a7e
Tu Nguyen <127782708+tunguyenn99@users.noreply.github.com>
2026-03-10 01:28:53 +0700

    Time: 890 ms (30.03%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT 
     u.name, 
-    COALESCE(SUM(r.distance), 0) AS travelled_distance
+    IFNULL(SUM(r.distance), 0) AS travelled_distance
 FROM 
     Users u
 LEFT JOIN 

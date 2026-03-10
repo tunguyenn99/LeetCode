72a2494b8ec5d6c0a50a41a820ec8667e25c6ef6
Tu Nguyen <127782708+tunguyenn99@users.noreply.github.com>
2026-03-10 20:01:52 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,8 +1,8 @@
 SELECT 
-    request_at AS Day, 
-    ROUND(AVG(status != 'completed'), 2) AS 'Cancellation Rate'
+    request_at AS "Day" 
+    , ROUND(AVG(status != 'completed'), 2) AS "Cancellation Rate"
 FROM Trips
 WHERE client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
   AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
   AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
-GROUP BY request_at;
+GROUP BY request_at

64ceda0af5a6e4f0a9da90f3fcfeaeeec60b761b
Tu Nguyen <127782708+tunguyenn99@users.noreply.github.com>
2026-03-10 20:06:13 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,9 +1,24 @@
 # Write your MySQL query statement below
+WITH prep AS 
+(
+    SELECT 
+        t.id
+        , t.status 
+        , t.request_at
+    FROM Trips t
+    INNER JOIN Users u1 
+        ON t.client_id = u1.users_id 
+            AND u1.role = 'client'
+            AND u1.banned = 'No'
+    INNER JOIN Users u2 
+        ON t.driver_id = u2.users_id 
+            AND u2.role = 'driver'
+            AND u2.banned = 'No'
+    WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'            
+)
 SELECT 
-    request_at AS "Day" 
-    , ROUND(AVG(status != 'completed'), 2) AS "Cancellation Rate"
-FROM Trips
-WHERE client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
-  AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
-  AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    request_at AS "Day"
+    , ROUND( 1.00 * COUNT(CASE WHEN status != 'completed' THEN id END) / COUNT(id) , 2) AS "Cancellation Rate"
+FROM prep
 GROUP BY request_at
+ORDER BY request_at
